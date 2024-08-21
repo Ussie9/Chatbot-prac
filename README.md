b9e3818f20def5013ac1521ac978c06949e1ad13
Ussie9 <usiyolise9@gmail.com>
2024-08-21 06:50:50 +0200

    chatbot complete

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Chatbot-prac
+
+This project acts as an introduction to Machine Learning. A lot of people tend to misunderstand the concept but with a few examples hopefully you'll be able to learn something new.
+
+Before we start, make sure that you have a working environment and that you're using Visual Studio.
+Create a python file (you may come with any name you like). On my GitHub server you should find a file called my_app.py, copy the code an paste it in your new python file
+
+Create a text file, this is where you'll store data whhich will be retrieved by the NLP. Copy the code on my text file (machine.txt) on GitHub and paste it on your new text file. You should also find a requirements file which contains modules which you'll need to execute the code.
+
+Now this ChatBot is very basic and can only answer three :
+                                           "what is machine learning\n"
+                                           "how to implement machine learning\n"
+                                           "differences between machine learning and neural networks"
+If a user goes beyond this scope they will encounter an error management code (basicaly a reply that the system doesn't have enough information on athat topic)
---
+++ my_app.py
@@ -0,0 +1,74 @@
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+
+my_doc = open("machine.txt", encoding="utf8")
+text = my_doc.read()
+my_doc = nlp(text)
+
+
+async def state0_handler(update, context):
+    doc = nlp(update.message.text)
+    reply = ''
+
+    pattern1 = [{"TEXT" : "what"},{"LEMMA" : "machine"} , {"POS" : "learn"}]
+    matcher.add("ML_DEFINITION_PATTERN" , [pattern1])
+    matched_what = matcher(doc)
+
+    pattern2 = [{"TEXT" : "how"} , {"LEMMA" : "machine"} , {"POS" : "learn"}]
+    matcher.add("ML_FUNCTION_PATTERN" , [pattern2])
+    matched_how = matcher(doc)
+
+    pattern3 = [{"TEXT" : "deep", "OP":"?"} ,{"TEXT" : "neural"},{"TEXT" : "machine"}, {"POS" : "learn"}]
+    matcher.add("ML_NEURAL_DIFF_PATTERN" , [pattern3])
+    matched_diff = matcher(doc)
+
+    if matched_what != 0:
+        print(my_doc[0])
+    elif matched_how != 0:
+        print(my_doc[3])
+    elif matched_diff != 0:
+        print(my_doc[16])
+    else :
+        await update.message.reply_text(random.choice([
+            "I didn't get the question, could you repeat it" ,
+            "Unfortunately I currently don't have enough information on that" ,
+        ]))
+    if reply:
+        await update.message.reply_text(reply)
+        # most likely, a different state needs to be returned
+    return 'STATE0'
+
+
+async def start(update, context):
+    await update.message.reply_text("Good day, how may I assist you")
+    return
+
+
+async def cancel(update, context):
+    await update.message.reply_text("Goodbye")
+    return ConversationHandler.END
+
+
+async def help(update, context):
+    return await update.message.reply_text("Try these most common questions:\n"
+                                           "what is machine learning\n"
+                                           "how to implement machine learning\n"
+                                           "differences between machine learning and neural networks")
+
+
+def main():
+    application = Application.builder().token('TOKEN').build()
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler(['start'], start)],
+        states={
+            'STATE0': [MessageHandler(filters.TEXT & ~filters.COMMAND, state0_handler)],
+        },
+        fallbacks=[CommandHandler(['cancel'], cancel),
+                   CommandHandler('help', help)]
+    )
+    application.add_handler(conv_handler)
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
